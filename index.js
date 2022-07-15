77ae580cff2616dc3ef06df0e33551124fa4eaed
MonsterBasket <blazza1@hotmail.com>
2022-07-15 17:07:51 +1000

    test complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+function appendCat(name){
+    let cats2 = [];
+    return cats2 = [...cats, name];
+}
+function prependCat(name){
+    let cats2 = [];
+    return cats2 = [name, ...cats];
+}
+function removeLastCat(){
+    let cats2 = [];
+    return cats.slice(0,cats.length -1);    
+}
+function removeFirstCat(){
+    let cats2 = [];
+    return cats.slice(1);    
+}
